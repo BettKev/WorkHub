1e9d3221c623c04bf26b13c790b0aac163af2142
Kevin Bett <prodigy4614@gmail.com>
2025-09-12 12:30:24 +0300

    update environment variables

--- db.py
+++ db.py
@@ -5,8 +5,8 @@ from supabase import create_client, Client
 
 # Load environment variables
 load_dotenv()
-url = os.getenv("SUPABASE_URL")
-key = os.getenv("SUPABASE_KEY")
+url = os.getenv("SUPABASE_URL") or "https://thkmxwkyuhxlwsgxllyg.supabase.co"
+key = os.getenv("SUPABASE_KEY") or "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRoa214d2t5dWh4bHdzZ3hsbHlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc1NDM1MjgsImV4cCI6MjA3MzExOTUyOH0.HzlNg7YIrGWvpp2l5dQfRzExNS6rq0fDvj2IHFzbpVg"
 
 if not url or not key:
     raise ValueError("❌ Missing SUPABASE_URL or SUPABASE_KEY in environment")
